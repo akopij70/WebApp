f4998875d55685970aa9dff7914dcf91603617f6
akopij70 <a.kopij70@gmail.com>
2023-04-26 19:25:36 +0200

    Dodaj pliki projektów.

---
+++ WebApp/Migrations/20230426170243_init.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Beer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    voltage = table.Column<float>(type: "real", nullable: false),
+                    volume = table.Column<float>(type: "real", nullable: false),
+                    price = table.Column<float>(type: "real", nullable: false),
+                    DeadLine = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beer");
+        }
+    }
+}
---
+++ WebApp/Models/Beer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace WebApp.Models
+{
+    public class Beer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public float voltage { get; set; }
+        public float volume { get; set; }
+        public float price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DeadLine { get; set; }
+    }
+}
