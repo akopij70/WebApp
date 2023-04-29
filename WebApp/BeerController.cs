4f0aff7fc720cd5b36d04c44e8d39fb6fd5dcee8
Kacper Wojtysiak <259262@student.pwr.edu.pl>
2023-04-29 14:35:15 +0200

    controller z funskcja szukaj

---
+++ WebApp/BeerController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp
+{
+    public class BeerController : Controller
+    {
+        private readonly WebAppContext _context;
+
+        public BeerController(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SearchById()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchById(int id)
+        {
+            Beer? beer = _context.Beer.Find(id);
+
+            if (beer == null)
+            {
+                ViewBag.Message = "Nie znaleziono piwa o podanym Id.";
+                return View();
+            }
+
+            return View("BeerDetails", beer);
+        }
+
+    }
+}
